ac479678c9659ad40beea98f8b5ec6ca09949f39
Javier García Montero <36073632+jgarmonedu@users.noreply.github.com>
2024-05-21 18:20:01 +0200

    General configuration stage

--- code/changame/resources/js/app.js
+++ code/changame/resources/js/app.js
@@ -1,4 +1,10 @@
+import jQuery from 'jquery';
+window.jQuery = window.$ = jQuery;
+
 import './bootstrap';
+import * as bootstrap from 'bootstrap';
+
+import 'laravel-datatables-vite';
 
 import Alpine from 'alpinejs';
 
